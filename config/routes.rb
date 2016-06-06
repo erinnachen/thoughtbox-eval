f0beb886d36e36047cb999e7992890de07ab79e7
Erinna Chen <erinna.chen@gmail.com>
2016-06-06 09:56:20 -0600

    User can signup with email and password

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: "dashboard#show"
+  resources :users, only: [:new, :create]
+  resources :links, only: [:index]
 end
---
+++ spec/features/user_can_sign_up_spec.rb
@@ -0,0 +1,23 @@
+# If I click "Sign Up", I should be taken to a user form where I can enter an email address, a password, and a password confirmation.
+#
+# I cannot sign up with an email address that has already been used.
+# Password and confirmation must match.
+# Upon submitting this information, I should be logged in via a cookie and redirected to the "Links Index" page.
+require 'rails_helper'
+
+RSpec.feature "User signs up for account" do
+  scenario "on the links index page" do
+    visit '/'
+    click_on "Sign up"
+
+    expect(current_path).to eq "/users/new"
+    fill_in "Email", with: "funthoughts@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_on "Create account"
+
+    expect(current_path).to eq "/links"
+    expect(page).to have_content "You have signed up!"
+    expect(page).to have_content "All the links for funthoughts@example.com"
+  end
+end
